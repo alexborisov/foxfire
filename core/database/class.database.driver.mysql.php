949ce5502bb83d0a8ff4a3167e228b793b6d31c6
foxly <carlroett@gmail.com>
2012-12-14 18:36:13 -0700

    Migrating class wp_db to FOX cache driver

--- core/database/class.database.driver.mysql.php
+++ core/database/class.database.driver.mysql.php
@@ -17,22 +17,18 @@
 class FOX_db_driver_mysql {
 
 
-	var $num_queries = 0;		// int	    | Amount of queries made
-	var $num_rows = 0;		// int	    | Count of rows returned by previous query
-	var $rows_affected = 0;		// int	    | Count of affected rows by previous query
-	var $insert_id = 0;		// int	    | The ID generated for an AUTO_INCREMENT column by the previous query (usually INSERT).
-	var $last_query;		// array    | Saved result of the last query made
-	var $last_result;		// array/null | Results of the last query made
-	var $col_info;			// array    | Saved info on the table column
-	var $save_queries;		// bool	    | Save queries for debugging.
-	var $queries;			// array    | Saved queries that were executed
-	var $ready = false;		// bool	    | Whether the database queries are ready to start executing.
-	var $charset;			// string   | Database table columns charset
-	var $collate;			// string   | Database table columns collate
-	var $real_escape = false;	// bool	    | Whether to use mysql_real_escape_string
-	var $dbuser;			// string   | Database Username
-	var $func_call;			// string   | A textual description of the last query/get_row/get_var call
-	var $dbh;			// int	    | Database handle as returned by PHP's MySQL class
+	var $rows_affected = 0;		// int		| Count of affected rows by previous query
+	var $insert_id = 0;		// int		| The ID generated for an AUTO_INCREMENT column by the previous query (usually INSERT).
+	var $last_result;		// array/null	| Results of the last query made
+
+	var $db_host;			// string	| Database Host
+	var $db_name;			// string	| Database Name
+	var $db_user;			// string	| Database User	Name
+	var $db_pass;			// string	| Database Password
+	
+	var $charset;			// string	| Database table columns charset
+	
+	var $dbh;			// int		| Database handle as returned by PHP's MySQL class
 
 
 	// #################################################################################################### //
@@ -41,23 +37,28 @@ class FOX_db_driver_mysql {
 	/**
 	 * Connects to the database server and selects a database
 	 *
-	 * @version 3.1
-	 * @since 0.1
-	 * @param string $dbuser MySQL database user
-	 * @param string $dbpassword MySQL database password
-	 * @param string $dbname MySQL database name
-	 * @param string $dbhost MySQL database host
+         * @version 1.0
+         * @since 1.0
+	 * 
+         * @param array $ctrl | Control parameters
+	 *	=> VAL @param string $db_host | Database host
+	 *	=> VAL @param string $db_name | Database name
+	 *	=> VAL @param string $db_user | Database user name
+	 *	=> VAL @param string $db_pass | Database password	 
+	 *	=> VAL @param string $charset | Database charset
+	 * 
+	 * @return bool | Exception on failure. True on success.
 	 */
+	
 	function __construct($args){
 
 	    
 		$args_default = array(
-					'db_host'=>'',	
-		    			'db_name'=>'',
-					'db_user'=>'',
-					'db_pass'=>'',
 					'charset'=>'utf8'	
 		);
+		
+		$args = wp_parse_args($args, $args_default);	
+		
 		
 		if(!FOX_sUtil::keyExists('db_host', $args)){
 
@@ -128,25 +129,25 @@ class FOX_db_driver_mysql {
 		// of the old 'SET NAMES %s' + 'COLLATE %s' method
 		
 		mysql_set_charset($this->charset, $this->dbh);
-
-		$this->select($this->db_name, $this->dbh);
 		
 		
-	}
+		try {
+			$this->select($this->db_name, $this->dbh);
+		}
+		catch (BPM_exception $child) {
 
-
-
-	/**
-	 * PHP5 style destructor and will run when database object is destroyed.
-	 *
-	 * @version 3.1
-	 * @since 0.1
-	 * @return bool true
-	 */
-	function __destruct() {
+			throw new BPM_exception( array(
+				'numeric'=>6,
+				'text'=>"Error in self::select()",
+				'data'=>$args,
+				'file'=>__FILE__, 'line'=>__LINE__, 'method'=>__METHOD__,
+				'child'=>$child
+			));		    
+		}
+		
 		return true;
+		
 	}
-
 
 
 	/**
@@ -293,7 +294,6 @@ class FOX_db_driver_mysql {
 	}
 
 
-
 	/**
 	 * Executes a SQL query and returns the row from the SQL result
 	 *
@@ -302,7 +302,7 @@ class FOX_db_driver_mysql {
 	 * @param string $query | SQL query as string
 	 * @param int $y Optional. Row to return. Indexed from 0.
 	 * 
-	 * @return mixed Database query result in format specifed by $output or null on failure
+	 * @return string | Exception on failure. Database query result (as string) on success.
 	 */
 	
 	function get_row($query, $y=0 ){
@@ -337,84 +337,76 @@ class FOX_db_driver_mysql {
 
 
 	/**
-	 * Retrieve one column from the database.
+	 * Executes a SQL query and returns the column from the SQL result.If the SQL result contains  
+	 * more than one column, this function returns the column specified.
 	 *
-	 * Executes a SQL query and returns the column from the SQL result.
-	 * If the SQL result contains more than one column, this function returns the column specified.
-	 * If $query is null, this function returns the specified column from the previous SQL result.
-	 *
-	 * @version 3.1
-	 * @since 0.1
-	 * @param string|null $query Optional. SQL query. Defaults to previous query.
-	 * @param int $x Optional. Column to return. Indexed from 0.
-	 * @return array Database query result. Array indexed from 0 by SQL result row number.
+         * @version 1.0
+         * @since 1.0
+	 * @param string $query | SQL query as string
+	 * @param int $x | Column to return. Indexed from 0.
+	 * 
+	 * @return array | Exception on failure. Database query result (as array of strings) on success.
 	 */
-	function get_col( $query = null , $x = 0 ) {
+	function get_col($query, $x=0 ){
 
-		if ( $query )
-			$this->query( $query );
-
-		$new_array = array();
-		// Extract the column values
-		for ( $i = 0, $j = count( $this->last_result ); $i < $j; $i++ ) {
-			$new_array[$i] = $this->get_var( null, $x, $i );
+	    
+		try {
+			$this->query($query);
 		}
-		return $new_array;
+		catch (BPM_exception $child) {
+
+			throw new BPM_exception( array(
+				'numeric'=>1,
+				'text'=>"Error in self::query()",
+				'data'=>$query,
+				'file'=>__FILE__, 'line'=>__LINE__, 'method'=>__METHOD__,
+				'child'=>$child
+			));		    
+		}
+
+		$result = array();
+		
+		// Extract the column values
+		
+		for( $i = 0, $j = count($this->last_result); $i < $j; $i++ ) {
+		    
+			$result[$i] = $this->get_var(null, $x, $i);
+		}
+		
+		return $result;
+		
 	}
 
 
-
 	/**
-	 * Retrieve an entire SQL result set from the database (i.e., many rows)
-	 *
 	 * Executes a SQL query and returns the entire SQL result.
 	 *
-	 * @version 3.1
-	 * @since 0.1
-	 * @param string $query SQL query.
-	 * @param string $output Optional. Any of ARRAY_A | ARRAY_N | OBJECT | OBJECT_K constants. With one of the first three, return an array of rows indexed from 0 by SQL result row number.
-	 * 	Each row is an associative array (column => value, ...), a numerically indexed array (0 => value, ...), or an object. ( ->column = value ), respectively.
-	 * 	With OBJECT_K, return an associative array of row objects keyed by the value of each row's first column's value.  Duplicate keys are discarded.
-	 * @return mixed Database query results
+         * @version 1.0
+         * @since 1.0
+	 * @param string $query | SQL query as string
+	 * @return mixed | Exception on failure. Database query result on success.
 	 */
-	function get_results( $query = null, $output = OBJECT ) {
+	
+	function get_results($query){
 
-		$this->func_call = "\$db->get_results(\"$query\", $output)";
-
-		if ( $query )
-			$this->query( $query );
-		else
-			return null;
-
-		$new_array = array();
-		if ( $output == OBJECT ) {
-			// Return an integer-keyed array of row objects
-			return $this->last_result;
-		} elseif ( $output == OBJECT_K ) {
-			// Return an array of row objects with keys from column 1
-			// (Duplicates are discarded)
-			foreach ( $this->last_result as $row ) {
-				$key = array_shift( $var_by_ref = get_object_vars( $row ) );
-				if ( ! isset( $new_array[ $key ] ) )
-					$new_array[ $key ] = $row;
-			}
-			return $new_array;
-		} elseif ( $output == ARRAY_A || $output == ARRAY_N ) {
-			// Return an integer-keyed array of...
-			if ( $this->last_result ) {
-				foreach( (array) $this->last_result as $row ) {
-					if ( $output == ARRAY_N ) {
-						// ...integer-keyed row arrays
-						$new_array[] = array_values( get_object_vars( $row ) );
-					} else {
-						// ...column name-keyed row arrays
-						$new_array[] = get_object_vars( $row );
-					}
-				}
-			}
-			return $new_array;
+	    
+		try {
+			$this->query($query);
 		}
-		return null;
+		catch (BPM_exception $child) {
+
+			throw new BPM_exception( array(
+				'numeric'=>1,
+				'text'=>"Error in self::query()",
+				'data'=>$query,
+				'file'=>__FILE__, 'line'=>__LINE__, 'method'=>__METHOD__,
+				'child'=>$child
+			));		    
+		}
+
+		
+		return $this->last_result;
+
 	}
 
 
